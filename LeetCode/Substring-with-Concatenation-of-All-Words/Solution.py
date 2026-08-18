f900fa2790bf04738c12e350a1246a3ff719be28
Alla Sesha Anjana Devi <devinaidu2006@gmail.com>
2026-08-18 22:09:26 +0530

    Add solution for Substring with Concatenation of All Words

--- LeetCode/Substring-with-Concatenation-of-All-Words/Solution.py
+++ LeetCode/Substring-with-Concatenation-of-All-Words/Solution.py
@@ -1,31 +1,31 @@
-class Solution:
-    def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        word_freq = defaultdict(int)
-        for word in words:
-            word_freq[word] += 1
-
-        word_len = len(words[0])
-        # words of the same length
-        window = len(words) * word_len
-        ans = []
-
-        for i in range(len(s) - window + 1):
-            substr_freq = defaultdict(int)
-            j = i
-
-            while j < i + window:
-                current = s[j : j + word_len]
-                if current not in word_freq:
-                    break
-
-                substr_freq[current] += 1
-                if substr_freq[current] > word_freq[current]:
-                    break
-
-                j += word_len
-            
-            if j == i + window:
-                ans.append(i)
-
-        return ans
-        
+1class Solution:
+2    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+3        word_freq = defaultdict(int)
+4        for word in words:
+5            word_freq[word] += 1
+6
+7        word_len = len(words[0])
+8        # words of the same length
+9        window = len(words) * word_len
+10        ans = []
+11
+12        for i in range(len(s) - window + 1):
+13            substr_freq = defaultdict(int)
+14            j = i
+15
+16            while j < i + window:
+17                current = s[j : j + word_len]
+18                if current not in word_freq:
+19                    break
+20
+21                substr_freq[current] += 1
+22                if substr_freq[current] > word_freq[current]:
+23                    break
+24
+25                j += word_len
+26            
+27            if j == i + window:
+28                ans.append(i)
+29
+30        return ans
+31        
