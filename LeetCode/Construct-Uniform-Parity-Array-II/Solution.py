b1ff719df486e53ffdc2263e0c806be0f1edd2fe
Alla Sesha Anjana Devi <devinaidu2006@gmail.com>
2026-09-03 07:42:38 +0530

    Add solution for Construct Uniform Parity Array II

--- LeetCode/Construct-Uniform-Parity-Array-II/Solution.py
+++ LeetCode/Construct-Uniform-Parity-Array-II/Solution.py
@@ -1,5 +1,10 @@
-For each index i, 
-you must choose exactly one of the following (in any order):
-
-nums2[i] = nums1[i]​​​​​​​
-nums2[i] = nums1[i]-nums1[j],  (j!=i && nums1[i] - nums1[j] >= 1)
+1class Solution:
+2    def uniformArray(self, a):
+3        mn = float('inf')
+4        oddCnt = 0
+5        for x in a:
+6            mn = min(mn, x)
+7            if x % 2 == 1:
+8                oddCnt += 1
+9        # min Element is ODD(remaining even > min) or All Even!
+10        return mn % 2 == 1 or oddCnt == 0
